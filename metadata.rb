528cd0905300876ea1a16c43e698f293066e87f9
Scott Ford <scott.ford@lacework.net>
2019-11-27 12:27:41 -0800

    updating metadata with info, links, and supporting os
    Signed-off-by: Scott Ford <scott.ford@lacework.net>

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,22 @@
 name 'chef-lacework'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Lacework, Inc.'
+maintainer_email 'humans@lacework.net'
 license 'All Rights Reserved'
 description 'Installs/Configures chef-lacework'
 version '0.1.0'
 chef_version '>= 14.0'
+%w( amazon centos fedora debian oracle redhat suse opensuse ubuntu ).each do |os|
+  supports os
+end
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/chef-lacework/issues'
+issues_url 'https://github.com/scottford-lw/chef-lacework/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/chef-lacework'
+source_url 'https://github.com/scottford-lw/chef-lacework'
