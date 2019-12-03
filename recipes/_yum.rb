f2debf4d2b7d05fa5e877f9fa657091af5e0349b
Scott Ford <scott.ford@lacework.net>
2019-12-02 16:06:19 -0800

    updating kitchen.yml with new suite for yum. updating install with case statement for attribute. update _yum.rb with repo and package
    Signed-off-by: Scott Ford <scott.ford@lacework.net>

--- recipes/_yum.rb
+++ recipes/_yum.rb
@@ -3,3 +3,14 @@
 # Recipe:: _yum
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+yum_repository 'packages-lacework-prod' do
+  action :create
+  baseurl 'https://packages.lacework.net/RPMS/x86_64'
+  gpgkey 'https://packages.lacework.net/keys/RPM-GPG-KEY-lacework'
+  gpgcheck true
+  enabled true
+end
+
+package 'lacework' do
+  action :install
+end
--- recipes/install.rb
+++ recipes/install.rb
@@ -3,4 +3,12 @@
 # Recipe:: install
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-include_recipe 'chef-lacework::_script'
+
+case node['chef-lacework']['install_method']
+when 'script'
+  include_recipe 'chef-lacework::_script'
+when 'yum'
+  include_recipe 'chef-lacework::_yum'
+when 'apt'
+  include_recipe 'chef-lacework::_apt'
+end
