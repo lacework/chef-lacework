74fac8282a288b3dac08c73b0fbd560fda6c2664
Scott Ford <scott.ford@lacework.net>
2019-12-03 10:27:13 -0800

    updating apt for debian
    Signed-off-by: Scott Ford <scott.ford@lacework.net>

--- recipes/_apt.rb
+++ recipes/_apt.rb
@@ -3,3 +3,32 @@
 # Recipe:: _apt
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+case node['platform']
+when 'ubuntu'
+  apt_repository 'packages-lacework-prod' do
+    uri "https://packages.lacework.net/DEB/#{node['platform']}/#{node['platform_version']}"
+    arch 'amd64'
+    distribution node['lsb']['codename']
+    key ['18E76630']
+    keyserver 'keyserver.ubuntu.com'
+    components ['main']
+    action :add
+  end
+when 'debian'
+  execute 'apt update --allow-releaseinfo-change'
+
+  apt_repository 'packages-lacework-prod' do
+    uri "https://packages.lacework.net/DEB/#{node['platform']}/#{node['platform_version'].to_i}"
+    arch 'amd64'
+    distribution node['lsb']['codename']
+    key ['18E76630']
+    keyserver 'keyserver.ubuntu.com'
+    components ['main']
+    action :add
+  end 
+end
+
+apt_package 'lacework' do
+  action :install
+end
