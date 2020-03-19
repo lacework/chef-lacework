f8cb64a7883e612b3bfbfb29ecebab0c9545f25c
Scott Ford <scott.ford@lacework.net>
2020-03-19 11:57:30 -0700

    refactor: Leverage install.sh from packages.lacework.net
    Refactors the default install method to pull install.sh from
    packages.lacework.net, updates the README, and adds Apache2 license
    attribute value was renamed from script to install_script
    updating copyright year in comments
    
    Signed-off-by: Scott Ford <scott.ford@lacework.net>

--- recipes/_deb.rb
+++ recipes/_deb.rb
@@ -2,10 +2,9 @@
 # Cookbook:: chef-lacework
 # Recipe:: _deb
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+# Copyright:: 2020, The Authors, All Rights Reserved.
 remote_file "#{node['chef-lacework']['package_dir']}/#{node['chef-lacework']['deb_package']['package_name']}" do
   source node['chef-lacework']['deb_package']['url']
-  checksum node['chef-lacework']['deb_package']['shasum']
 end
 
 dpkg_package node['chef-lacework']['deb_package']['package_name'] do
